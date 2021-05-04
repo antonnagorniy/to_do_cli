5ca9a1e5b6effcb638397e68e58839050637f4fe
kattaris <kattaris@protonmail.ch>
2021-05-04 16:11:52 +0300

    [+] TodoList

--- src/models.rs
+++ src/models.rs
@@ -1,4 +1,4 @@
-pub mod todo_item {
+pub mod items {
     use std::fmt;
     use std::fmt::Formatter;
 
@@ -8,9 +8,30 @@ pub mod todo_item {
         pub completed: char,
     }
 
+    impl TodoItem {
+        pub fn new(name: String, completed: char) -> TodoItem {
+            TodoItem{ name, completed }
+        }
+    }
+
     impl fmt::Display for TodoItem {
         fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
             write!(f, "[{}] - {}", self.completed, self.name)
+        }
+    }
+
+    #[derive(Debug)]
+    pub struct TodoList {
+        pub list: Vec<TodoItem>
+    }
+
+    impl TodoList {
+        pub fn new() -> TodoList {
+            return TodoList{ list: Vec::new() }
+        }
+
+        pub fn add(&mut self, item: TodoItem) {
+            self.list.push(item)
         }
     }
 }
